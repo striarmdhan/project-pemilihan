20293cb58446f5704c4e8e3e24577eba21e121ad
striarmdhan <@muhhamadsatriaramadhan1@gmail.com>
2026-01-19 20:40:31 +0700

    Force storage path to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Paksa Laravel menggunakan folder /tmp untuk storage di Vercel
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// Trik agar Laravel bisa nulis file (cache/view/log) di serverless
+$app->useStoragePath('/tmp');
+
+// Jalankan request
+$request = Illuminate\Http\Request::capture();
+$app->handleRequest($request);
